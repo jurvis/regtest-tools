1bfacc7c450ecb753d0d159ec15f39d28bf3a961
Jurvis Tan <jurvis@squareup.com>
2024-05-07 15:05:35 -0700

    remove log-filters arg

--- electrs/entrypoint.sh
+++ electrs/entrypoint.sh
@@ -4,7 +4,6 @@ exec bin/electrs \
     --network "regtest" \
     --cookie "bitcoin:local123" \
     --jsonrpc-import \
-    --log-filters INFO \
     --http-addr 0.0.0.0:9000 \
     --electrum-rpc-addr 0.0.0.0:60401 \
 	$@
